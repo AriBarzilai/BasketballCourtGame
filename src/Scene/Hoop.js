f802d5d62bc6aa64a8c06e4f08cfc02f0c395688
Netta Yaniv <netta.yaniv@runi.ac.il>
2025-06-19 23:53:21 +0300

    Added hoops- still require adjusments

--- src/Scene/Hoop.js
+++ src/Scene/Hoop.js
@@ -1,66 +1,167 @@
+import * as utils from '../utils.js'
 
 const POLE_HEIGHT = 10;
 const POLE_RADIUS = 0.1;
-const POLE_POSITION = { x: 0, y: POLE_HEIGHT / 2, z: -1.5 };
+const HOOP_HEIGHT = 10; 
+const BOARD_SCALE = 3;
+const RIM_RADIUS = 0.9;
 
-function BasketballHoops(scene) {  
+function BasketballHoops() {  
+    const hoopsGroup = new THREE.Group();
     
+    const leftHoop = createSingleHoop();
+    leftHoop.position.set(-45.78, 0, 0);
+    leftHoop.rotation.y = Math.PI / 2;
+
+    const rightHoop = createSingleHoop();
+    rightHoop.position.set(0, 0, 0);
+    rightHoop.rotation.y = -Math.PI / 2;
+    
+    hoopsGroup.add(leftHoop);
+    hoopsGroup.add(rightHoop);
+    
+    return {
+        object: hoopsGroup,
+        baseHeight: 0
+    };
+}
+
+function createSingleHoop() {
     const hoop = new THREE.Group();
 
     const pole = createBasketballPole();
     const board = createBasketballBoard();
     const supportArm = createSupportArm();
+    const rim = createBasketballRim();
+    // const net = createBasketballNet();
 
-    hoop.add(pole, board, supportArm);
+    hoop.add(pole, board, supportArm, rim);
     return hoop;
 }
 
 function createBasketballPole() {
         const poleGeometry = new THREE.CylinderGeometry(POLE_RADIUS, POLE_RADIUS, POLE_HEIGHT, 8);
-        const poleMaterial = new THREE.MeshPhongMaterial({ color: 0x4169E1});
+        const poleMaterial = new THREE.MeshPhongMaterial({ color: utils.COLORS.BLACK});
         
         const pole = new THREE.Mesh(poleGeometry, poleMaterial);
         
-        pole.position.set(POLE_POSITION.x, POLE_POSITION.y, POLE_POSITION.z);
+        pole.position.set(0, POLE_HEIGHT / 2, -2);
         pole.castShadow = true;
         pole.receiveShadow = true;
         return pole;
 }
 
-function createBasketballBoard(){
-    const backboardGeometry = new THREE.BoxGeometry(1.8, 1.05, 0.05);
-    const backboardMaterial = new THREE.MeshPhongMaterial({ 
-        color: 0xffffff, 
-        transparent: true,
-        opacity: 0.9 
-    });
-    const backboard = new THREE.Mesh(backboardGeometry, backboardMaterial);
-    const backboardPosition = {
-        x: POLE_POSITION.x,
-        y: POLE_POSITION.y + 4.5,
-        z: POLE_POSITION.z + 1.5
-    };
-    backboard.position.set(backboardPosition.x, backboardPosition.y, backboardPosition.z);
-    backboard.castShadow = true;
-    backboard.receiveShadow = true;
-    return backboard;
-}
-
 function createSupportArm() {
     const armGeometry = new THREE.BoxGeometry(0.15, 0.15, 1.8);
-    const armMaterial = new THREE.MeshPhongMaterial({ color: 0x4169E1 });
+    const armMaterial = new THREE.MeshPhongMaterial({ color: utils.COLORS.BLACK }); 
     const arm = new THREE.Mesh(armGeometry, armMaterial);
-        
-    const armPosition = {
-            x: POLE_POSITION.x,
-            y: POLE_POSITION.y + 4.5, 
-            z: POLE_POSITION.z + 0.9 
-        };
-    arm.position.set(armPosition.x, armPosition.y, armPosition.z);
+    
+    arm.position.set(0, HOOP_HEIGHT - 0.5, -1);
     arm.castShadow = true;
     return arm;
 }
 
+function createBasketballBoard(){
+    const boardGroup = new THREE.Group();
 
+    const backboardGeometry = new THREE.BoxGeometry(1.83 * BOARD_SCALE, 1.22 * BOARD_SCALE, 0.05);
+    const backboardMaterial = new THREE.MeshPhongMaterial({ 
+        color: utils.COLORS.WHITE, 
+        transparent: true,
+        opacity: 0.3
+    });
+    const backboard = new THREE.Mesh(backboardGeometry, backboardMaterial);
+    boardGroup.add(backboard);
+    
+    const borderThickness = 0.012 * BOARD_SCALE;
+    const boardWidth = 1.83 * BOARD_SCALE;
+    const boardHeight = 1.22 * BOARD_SCALE;
+
+    const topBorder = new THREE.Mesh(
+        new THREE.BoxGeometry(boardWidth, borderThickness, 0.06),
+        utils.MATERIALS.COURT_LINES
+    );
+    topBorder.position.set(0, boardHeight/2 - borderThickness/2, 0.001);
+    boardGroup.add(topBorder);
+    
+    const bottomBorder = new THREE.Mesh(
+        new THREE.BoxGeometry(boardWidth, borderThickness, 0.06),
+        utils.MATERIALS.COURT_LINES
+    );
+    bottomBorder.position.set(0, -boardHeight/2 + borderThickness/2, 0.001);
+    boardGroup.add(bottomBorder);
+    
+    const leftBorder = new THREE.Mesh(
+        new THREE.BoxGeometry(borderThickness, boardHeight, 0.06),
+        utils.MATERIALS.COURT_LINES
+    );
+    leftBorder.position.set(-boardWidth/2 + borderThickness/2, 0, 0.001);
+    boardGroup.add(leftBorder);
+    
+    const rightBorder = new THREE.Mesh(
+        new THREE.BoxGeometry(borderThickness, boardHeight, 0.06),
+        utils.MATERIALS.COURT_LINES
+    );
+    rightBorder.position.set(boardWidth/2 - borderThickness/2, 0, 0.001);
+    boardGroup.add(rightBorder);
+    
+
+    const innerThickness = 0.008 * BOARD_SCALE; 
+    const squareWidth = 0.61 * BOARD_SCALE; 
+    const squareHeight = 0.457 * BOARD_SCALE; 
+    
+    const innerTop = new THREE.Mesh(
+        new THREE.BoxGeometry(squareWidth, innerThickness, 0.06),
+        utils.MATERIALS.COURT_LINES
+    );
+    innerTop.position.set(0, squareHeight/2, 0.002);
+    boardGroup.add(innerTop);
+    
+    const innerBottom = new THREE.Mesh(
+        new THREE.BoxGeometry(squareWidth, innerThickness, 0.06),
+        utils.MATERIALS.COURT_LINES
+    );
+    innerBottom.position.set(0, -squareHeight/2, 0.002);
+    boardGroup.add(innerBottom);
+    
+    const innerLeft = new THREE.Mesh(
+        new THREE.BoxGeometry(innerThickness, squareHeight, 0.06),
+        utils.MATERIALS.COURT_LINES
+    );
+    innerLeft.position.set(-squareWidth/2, 0, 0.002);
+    boardGroup.add(innerLeft);
+    
+    const innerRight = new THREE.Mesh(
+        new THREE.BoxGeometry(innerThickness, squareHeight, 0.06),
+        utils.MATERIALS.COURT_LINES
+    );
+    innerRight.position.set(squareWidth/2, 0, 0.002);
+    boardGroup.add(innerRight);
+    
+    boardGroup.position.set(0, HOOP_HEIGHT - 0.5, -0.15);
+    boardGroup.children.forEach(child => {
+        child.castShadow = true;
+        child.receiveShadow = true;
+    });
+    
+    return boardGroup;
+}
+
+function createBasketballRim() {
+    const rimGeometry = new THREE.TorusGeometry(RIM_RADIUS, 0.02, 8, 50);
+    const rimMaterial = new THREE.MeshPhongMaterial({ color: 0xFF6600 });
+    const rim = new THREE.Mesh(rimGeometry, rimMaterial);
+    
+    const squareHeight = 0.457;
+    const rimY = HOOP_HEIGHT - 0.5 + (-squareHeight/2);    
+    rim.position.set(0, rimY, 0.30);
+    rim.rotation.x = Math.PI / 2;
+    rim.castShadow = true;
+    return rim;
+}
+
+// function createBasketballNet() {
+//     return;
+// }
 
 export { BasketballHoops };
